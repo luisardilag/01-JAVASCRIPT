4c210e6a649a503c816a781d8c4847cfbbb23ed7
Luis Ardila <luisardilag@gmail.com>
2025-11-27 17:08:12 -0300

    update(array): Practice Syntax Arr3

--- Codeacademy-projects/ARRAYS/Practices-syntax-II.js
+++ Codeacademy-projects/ARRAYS/Practices-syntax-II.js
@@ -58,3 +58,89 @@ const dinner = [{name: 'hamburger', source: 'meat'}, {name: 'cheese', source: 'd
 
 console.log(isTheDinnerVegan(dinner))
 // Should print false
+
+
+/*
+==========================================================================
+*/
+
+// Use .sort(a, b) array method:
+const speciesArray = [ 
+  {speciesName:'shark', numTeeth:50},
+  {speciesName:'dog', numTeeth:42},
+  {speciesName:'alligator', numTeeth:80},
+  {speciesName:'human', numTeeth:32}
+];
+
+const sortSpeciesByTeeth = (animal) => {
+   return animal.sort((a, b) => a.numTeeth - b.numTeeth );
+  console.log(animal);
+}
+
+console.log(sortSpeciesByTeeth(speciesArray))
+
+// Should print:
+// [ { speciesName: 'human', numTeeth: 32 },
+//   { speciesName: 'dog', numTeeth: 42 },
+//   { speciesName: 'shark', numTeeth: 50 },
+//   { speciesName: 'alligator', numTeeth: 80 } ]
+
+
+/*
+==========================================================================
+*/
+
+// Use .findIndex() array method:
+const findMyKeys = (arr) => {
+  return arr.findIndex(e => e === "keys")
+}
+
+const randomStuff = ['credit card', 'screwdriver', 'receipt', 'gum', 'keys', 'used gum', 'plastic spoon'];
+
+console.log(findMyKeys(randomStuff))
+// Should print 4
+
+
+
+/*
+==========================================================================
+*/
+
+const dogFactory = (name, breed, weight) => {
+  return {
+    _name: name,
+    _breed: breed,
+    _weight: weight,
+
+    get name() {
+      return this._name;
+    },
+    get breed() {
+      return this._breed;
+    },
+    get weight() {
+      return this._weight;
+    },
+    set name(newName) {
+      this._name = newName;
+    },
+    set breed(newBreed) {
+      this._breed = newBreed;
+    },
+    set weight(newWeight) {
+      this._weight = newWeight;
+    },
+    bark() {
+      return "ruff! ruff!";
+    },
+    eatTooManyTreats() {
+      this._weight++;
+    }
+    
+  };
+};
+
+const newDog = dogFactory('Joe', 'Pug', 27);
+newDog.bark();
+newDog.eatTooManyTreats();
+// Should return { name: 'Joe', breed: 'Pug', weight: 27 }
